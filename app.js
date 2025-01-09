1a50093c942b7b099273ccc2d7a4c72c82894665
JustUrn <chiraratana@gmail.com>
2025-01-09 15:19:59 +0700

    add products files

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const path = require('path');   
+const path = require('path');
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -14,18 +15,18 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res)=>{
-    res.send("Hello World, I'm g");
+productRouter.route("/").get((req, res) => {
+    res.render("products", products);
 });
 
-productRouter.route("/1").get((req, res)=>{
+productRouter.route("/1").get((req, res) => {
     res.send("Hello World, I'm wan");
 });
 
 app.use("/products", productRouter)
 
 app.get("/", (req, res) => {
-    res.render('index', {username: 'Earneiei', customers: ["Aldrick","Floyd","Noel"]}) ;
+    res.render('index', { username: 'Earneiei', customers: ["Aldrick", "Floyd", "Noel"] });
 })
 
 app.listen(PORT, () => {
