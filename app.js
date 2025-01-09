983e494d24c98bacb84e696c20ddfcc30f48d269
JustUrn <chiraratana@gmail.com>
2025-01-09 12:34:01 +0700

    route test

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');   
+const productRouter = express.Router();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res)=>{
+    res.send("Hello World, I'm g");
+});
+
+productRouter.route("/1").get((req, res)=>{
+    res.send("Hello World, I'm wan");
+});
+
+app.use("/products", productRouter)
 
 app.get("/", (req, res) => {
     res.render('index', {username: 'Earneiei', customers: ["Aldrick","Floyd","Noel"]}) ;
